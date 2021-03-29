bb1f1944d16afd8f59ab97bc77cb4f627d4e1f2a
cipst <68118972+cipst@users.noreply.github.com>
2021-03-29 14:50:49 +0200

    create MutablePoint.java

---
+++ Lezione 4/MutablePoint.java
@@ -0,0 +1,53 @@
+class MutablePoint {
+
+    private double x = 0.0;
+    private double y = 0.0;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void moveTo(double dx, double dy) {
+        System.out.println("Coordinate prima della traslazione: (" + this.x + "," + this.y + ")");
+        this.x += dx;
+        this.y += dy;
+        System.out.println("Coordinate dopo la traslazione: (" + this.x + "," + this.y + ")");
+    }
+
+    /*
+     * public Point moveToImm(double dx, double dy) {
+     * System.out.println("Coordinate prima della traslazione: (" + this.x + "," +
+     * this.y + ")"); Point newPoint = new Point(this.x + dx, this.y + dy);
+     * System.out.println("Coordinate dopo la traslazione: (" + newPoint.getX() +
+     * "," + newPoint.getY() + ")"); return newPoint; }
+     */
+
+    public void rotate(int angle) {
+        System.out.println("Coordinate prima della rotazione: (" + this.x + "," + this.y + ")");
+        this.x = x * Math.cos(angle) - y * Math.sin(angle);
+        this.y = x * Math.sin(angle) + y * Math.cos(angle);
+        System.out.println("Coordinate dopo la rotazione: (" + this.x + "," + this.y + ")");
+    }
+
+    /*
+     * public Point rotateImm(int angle) {
+     * System.out.println("Coordinate prima della rotazione: (" + this.x + "," +
+     * this.y + ")"); Point newPoint = new Point(x * Math.cos(angle) - y *
+     * Math.sin(angle), x * Math.sin(angle) + y * Math.cos(angle));
+     * System.out.println("Coordinate dopo la rotazione: (" + newPoint.getX() + ","
+     * + newPoint.getY() + ")"); return newPoint; }
+     */
+
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow((this.x - p.getX()), 2) + Math.pow((this.y - p.getY()), 2));
+    }
+}
