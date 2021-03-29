0e0affcfc8d803447f4b4aab1dfa286a156a78be
cipst <68118972+cipst@users.noreply.github.com>
2021-03-29 14:53:26 +0200

    constructor & get methods

--- Lezione 4/MutableTriangle.java
+++ Lezione 4/MutableTriangle.java
@@ -1,3 +1,24 @@
-public class MutableTriangle{
+public class MutableTriangle {
+    private MutablePoint a;
+    private MutablePoint b;
+    private MutablePoint c;
 
-}//class
+    public MutableTriangle(MutablePoint a, MutablePoint b, MutablePoint c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }// constructor
+
+    public MutablePoint getA() {
+        return this.a;
+    }// get A
+
+    public MutablePoint getB() {
+        return this.b;
+    }// get B
+
+    public MutablePoint getC() {
+        return this.c;
+    }// get C
+
+}// class
