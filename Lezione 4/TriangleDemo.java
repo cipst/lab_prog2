2ea48dab51291a99170e1e2f493665a0ca969491
cipst <68118972+cipst@users.noreply.github.com>
2021-03-29 15:29:56 +0200

    update TriangleDemo.java

--- Lezione 4/TriangleDemo.java
+++ Lezione 4/TriangleDemo.java
@@ -1,4 +1,4 @@
-public class MutableTriangleDemo {
+public class TriangleDemo {
     public static void main(String[] args){
         MutableTriangle t = new MutableTriangle(new MutablePoint(0,0), new MutablePoint(29,14), new MutablePoint(9,22));
         System.out.println("Area= " + String.format("%.2f", t.area()));
