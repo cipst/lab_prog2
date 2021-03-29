5f92f569aec7c2b400b066e6933740e581df1c5a
cipst <68118972+cipst@users.noreply.github.com>
2021-03-29 15:58:30 +0200

    complete ImmutableTriangle.java

--- Lezione 4/ImmutableTriangle.java
+++ Lezione 4/ImmutableTriangle.java
@@ -1,3 +1,46 @@
 public class ImmutableTriangle {
-    
+    private ImmutablePoint a;
+    private ImmutablePoint b;
+    private ImmutablePoint c;
+
+    public ImmutableTriangle(ImmutablePoint a, ImmutablePoint b, ImmutablePoint c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }// constructor
+
+    public ImmutablePoint getA() {
+        return this.a;
+    }// get A
+
+    public ImmutablePoint getB() {
+        return this.b;
+    }// get B
+
+    public ImmutablePoint getC() {
+        return this.c;
+    }// get C
+
+    public ImmutableTriangle translate(double dx, double dy) {
+        return new ImmutableTriangle(this.a.moveTo(dx,dy), this.b.moveTo(dx, dy), this.c.moveTo(dx, dy));
+    }// translate
+
+    public ImmutableTriangle rotate(double angle) {
+        return new ImmutableTriangle(this.a.rotate(dx,dy), this.b.rotate(dx, dy), this.c.rotate(dx, dy));
+    }// rotate
+
+    public double perimeter() {
+        return this.a.distance(b) + this.b.distance(c) + this.a.distance(c);
+    }// perimeter
+
+    public double area() {
+        double AB = this.a.distance(b);
+        double BC = this.b.distance(c);
+        double AC = this.a.distance(c);
+
+        // Formula di Erone
+        double s = (AB + BC + AC) / 2;
+
+        return Math.sqrt(s * (s - AB) * (s - BC) * (s - AC));
+    }// area
 }
