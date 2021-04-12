c59f8367c58789d386dd090be027029210b3cb5a
Stefano <stefano.cipolletta@edu.unito.it>
2021-04-12 14:29:59 +0200

    method toArray implemented recursively and iterative
    TO DO: test them

--- Lezione 5/PilaDinamica.java
+++ Lezione 5/PilaDinamica.java
@@ -15,8 +15,8 @@ public class PilaDinamica {
         return this.top == null;
     }
 
-    // Ritorna true se la pila e' piena. 
-    // Poiche' la pila ha dimensione dinamica, non puo' mai 
+    // Ritorna true se la pila e' piena.
+    // Poiche' la pila ha dimensione dinamica, non puo' mai
     // essere piena, a meno di finire la memoria
     public boolean piena() {
         return false;
@@ -33,5 +33,38 @@ public class PilaDinamica {
         int tmp = top.getElem();
         top = top.getNext();
         return tmp;
+    }
+
+    public int[] toArray() {
+        assert !vuota() : "non si puo' chiamare il metodo toArray() se la pila e' vuota. ";
+        int[] a = new int[this.size()];
+        int i = 0;
+        for (Node tmp = top.getElem(); tmp != null; tmp = tmp.getNext()) {
+            a[i++] = tmp.getElem();
+        }
+        return a;
+    }
+
+    public int[] toArray(Node tmp, int size){
+        if(tmp == null){
+            return new int[size];
+        }else{
+            int[] a = toArray(tmp.getNext(), size+1);
+            a[size-1] = tmp.getElem();
+            return a;
+        }
+    }
+
+    public int[] toArrayWrap(){
+        assert !vuoto() : "non si puo' chiamare toArrayWrap() se la pila e' vuota. ";
+        return toArray(top, 0);
+    }
+
+    public int size() {
+        int size = 0;
+        for (Node tmp = top.getElem(); tmp != null; tmp = tmp.getNext()) {
+            size++;
+        }
+        return size;
     }
 }
