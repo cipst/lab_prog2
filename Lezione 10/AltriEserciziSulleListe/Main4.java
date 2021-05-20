ff36b0ef594275dc4f6f9b88fe11aa86554d4c06
Stefano <stefano.cipolletta@edu.unito.it>
2021-05-20 15:13:04 +0200

    Lezione 10 - Liste
    Main4.java
    addOdd (Node<Node<Integer>> ll)

--- Lezione 10/AltriEserciziSulleListe/Main4.java
+++ Lezione 10/AltriEserciziSulleListe/Main4.java
@@ -41,13 +41,13 @@
 
 import java.util.*;
 
-class Node<T>{
+class Node<T> {
     private T elem;
     private Node<T> next;
 
     public Node(T elem, Node<T> next) {
-        this.elem=elem;
-        this.next=next;
+        this.elem = elem;
+        this.next = next;
     }
 
     public T getElem() {
@@ -59,67 +59,66 @@ class Node<T>{
     }
 
     public void setElem(T elem) {
-        this.elem=elem;
+        this.elem = elem;
     }
+
     public void setNext(Node<T> next) {
-        this.next=next;
+        this.next = next;
     }
 
     @Override
     public String toString() {
         String res = "[";
-        res += elem==null?"[]":elem.toString();
+        res += elem == null ? "[]" : elem.toString();
         Node<T> p = next;
         while (p != null) {
-            res += ","+((p.elem == null?"[]":p.elem.toString()));
+            res += "," + ((p.elem == null ? "[]" : p.elem.toString()));
             p = p.next;
-        }   
+        }
         return res += "]";
-    }  
+    }
 }
-
-
 
 public class Main4 {
 
     private static void addOdd(Node<Node<Integer>> ll) {
-        /////////////////////////
-        //    DA COMPLETARE    //
-        /////////////////////////
+        for (Node<Node<Integer>> p = ll; p != null; p = p.getNext())
+            for (Node<Integer> q = p.getElem(); q != null; q = q.getNext())
+                if (q.getElem() % 2 == 0)
+                    q.setNext(new Node<Integer>(q.getElem() + 1, q.getNext()));
     }
 
-    //------ INIZIO CODICE PER IL TESTING ---------------------------------
+    // ------ INIZIO CODICE PER IL TESTING ---------------------------------
 
     public static void main(String[] args) {
-        test(crea(), "[]");//[]
-        test(crea(crea()), "[[]]");//[[]]
-        test(crea(crea(1,2),crea()), "[[1,2,3],[]]");//[[1,2],[]]
-        test(crea(crea(),crea(1,2)), "[[],[1,2,3]]");//[[],[1,2]]
-        test(crea(crea(1),crea(2)), "[[1],[2,3]]");//[[1],[2]]
-        test(crea(crea(1,2),crea(),crea(3,4,5,6,6)), "[[1,2,3],[],[3,4,5,5,6,7,6,7]]");//[[1,2],[],[3,4,5,6,6]]
+        test(crea(), "[]");// []
+        test(crea(crea()), "[[]]");// [[]]
+        test(crea(crea(1, 2), crea()), "[[1,2,3],[]]");// [[1,2],[]]
+        test(crea(crea(), crea(1, 2)), "[[],[1,2,3]]");// [[],[1,2]]
+        test(crea(crea(1), crea(2)), "[[1],[2,3]]");// [[1],[2]]
+        test(crea(crea(1, 2), crea(), crea(3, 4, 5, 6, 6)), "[[1,2,3],[],[3,4,5,5,6,7,6,7]]");// [[1,2],[],[3,4,5,6,6]]
     }
 
-
     private static void test(Node<Node<Integer>> ll, String atteso) {
-        String lista = (ll == null?"[]":ll.toString());
+        String lista = (ll == null ? "[]" : ll.toString());
         System.out.println("lista    : " + lista);
         addOdd(ll);
         Node<Node<Integer>> ottenuto = ll;
-        System.out.println("ottenuto : " + (ottenuto==null?"[]":ll));
+        System.out.println("ottenuto : " + (ottenuto == null ? "[]" : ll));
         System.out.println("atteso   : " + atteso);
-        System.out.println(((ottenuto != null && ottenuto.toString().equals(atteso)) || 
-                           (ottenuto == null && atteso.equals("[]"))) ? "=== OK" : "=== ERRORE");
+        System.out.println(
+                ((ottenuto != null && ottenuto.toString().equals(atteso)) || (ottenuto == null && atteso.equals("[]")))
+                        ? "=== OK"
+                        : "=== ERRORE");
         System.out.println();
     }
 
     @SafeVarargs
-    private static <T> Node<T> crea(T...v) {
+    private static <T> Node<T> crea(T... v) {
         Node<T> n = null;
         for (int i = v.length - 1; i >= 0; i--)
-            n = new Node<>(v[i],n);
+            n = new Node<>(v[i], n);
         return n;
     }
-    //------ FINE CODICE PER IL TESTING ----------------------------------
+    // ------ FINE CODICE PER IL TESTING ----------------------------------
 }
-
-
